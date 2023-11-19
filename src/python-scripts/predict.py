96c6a3c904f59486e3bf04038b3f57482eaad16d
Daniil Zatsepin <red-county@mail.ru>
2023-11-19 22:45:23 +0300

    New

--- src/python-scripts/predict.py
+++ src/python-scripts/predict.py
@@ -5,4 +5,9 @@ test = ITSASModel.load('.\\src\\models\\my_model.pkl')
 
 df = test.predict(n = sys.argv[1])
 
-print(df)
+out = df.to_json(orient="columns")
+
+with open('data.json', 'w') as f:
+    f.write(out)
+
+print("Ok!")
