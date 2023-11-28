f461f6d2bfe4eefe3cfb9cad1bd37e72d89db68c
Daniil Zatsepin <red-county@mail.ru>
2023-11-28 18:56:52 +0300

    Update scripts

--- src/python-scripts/predict.py
+++ src/python-scripts/predict.py
@@ -1,16 +1,9 @@
 from ITSAS import ITSASModel
 import sys
-from torch.multiprocessing import set_start_method
-try:
-     set_start_method('spawn')
-except RuntimeError:
-    pass
-import logging
-logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
 
 sys.stderr = open('error_log.txt', 'w')
 
-test = ITSASModel.load('models\my_model.pkl')
+test = ITSASModel.load('src\python-scripts\models\my_model.pkl')
 
 df = test.predict(n = int(sys.argv[1]))
 
@@ -18,6 +11,8 @@ df = test.predict(n = int(sys.argv[1]))
 df = df.pd_dataframe()
 
 # Преобразование DataFrame в JSON
-out = df.to_json(orient="records")
+out = df.to_json()
+
+sys.stdout = open('out_log.txt', 'w')
 
 print(out)
